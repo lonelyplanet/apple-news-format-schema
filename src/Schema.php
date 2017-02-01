cf5611166d2b53dc68ebf09978e086f6fd713585
Eric King <eric.king@lonelyplanet.com>
2017-02-01 15:42:40 -0600

    Added Schema class

---
+++ src/Schema.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Eric King <eric.king@lonelyplanet.com>
+ */
+
+namespace LonelyPlanet\AppleNewsFormat;
+
+/**
+ * This class provides some helper methods.
+ * It is a class so it can be autoloaded with Composer.
+ */
+class Schema
+{
+    public static function getPath()
+    {
+        return realpath( '../docs/schema.json' );
+    }
+
+    public static function getContents()
+    {
+        return file_get_contents( self::getPath() );
+    }
+
+    public static function toJSON()
+    {
+        return json_decode( self::getContents() );
+    }
+
+    public static function toArray()
+    {
+        return json_decode( self::getContents(), true );
+    }
+}
