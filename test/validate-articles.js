559c03bd88b9452e1a489f9308986e5580462933
Eric King <eric.king@lonelyplanet.com>
2017-02-02 13:22:37 -0600

    Updated tests to check invalid articles

--- test/validate-articles.js
+++ test/validate-articles.js
@@ -5,16 +5,27 @@ const assert = require('chai').assert;
 const glob = require('glob');
 const validate = require('jsonschema').validate;
 const schema = require('../docs/schema.json');
-const articles = path.join( process.cwd(), 'test', 'fixtures', '**', '*.json' );
+const validArticles = path.join( process.cwd(), 'test', 'fixtures', 'valid', '*.json' );
+const invalidArticles = path.join( process.cwd(), 'test', 'fixtures', 'invalid', '*.json' );
 const validateOptions = { nestedErrors: true };
 const inspectOptions = { showHidden: true, depth: null };
 
-describe('Apple News Format schema', function() {
-  describe('validate article', () => {
-    glob.sync(articles).forEach( article => {
+describe('Apple News Format schema', () => {
+  describe('Valid articles', () => {
+    glob.sync(validArticles).forEach( article => {
       it( path.basename(article), () => {
         const results = validate( require(article), schema, validateOptions );
         assert.isTrue( results.valid, util.inspect( results.errors, inspectOptions ) );
+      });
+    });
+  });
+
+  describe('Invalid articles', () => {
+    glob.sync(invalidArticles).forEach( article => {
+      it( path.basename(article), () => {
+        const results = validate( require(article), schema, validateOptions );
+        assert.isFalse( results.valid, `${path.basename(article)} is valid, but shouldn't be` );
+        assert.isAtLeast( results.errors.length, 1 );
       });
     });
   });
